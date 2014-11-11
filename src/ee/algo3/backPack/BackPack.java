c81182d0e475469ad3fce9c0c7d0401f78cc054a
Riuno91 <riuno91@gmail.com>
2014-11-11 10:31:35 +0200

    commit

--- src/ee/algo3/backPack/BackPack.java
+++ src/ee/algo3/backPack/BackPack.java
@@ -1,7 +1,25 @@
 package ee.algo3.backPack;
 
+import ee.algo3.items.Item;
+
+import java.util.ArrayList;
+
 /**
  * Created by Riuno on 11.11.2014.
  */
 public class BackPack {
+    private double weight;
+    private ArrayList<Item> items;
+
+    public BackPack() {
+        items = new ArrayList<Item>();
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
--- src/ee/algo3/items/Item.java
+++ src/ee/algo3/items/Item.java
@@ -1,7 +1,30 @@
 package ee.algo3.items;
 
+
 /**
  * Created by Riuno on 11.11.2014.
  */
 public class Item {
+    private double weight;
+    private double price;
+
+    public Item() {
+
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
